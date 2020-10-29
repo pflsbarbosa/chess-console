8831da760c053656a4726a52635c66de322753d2
Paulo Barbosa <pflsbarbosa@gmail.com>
2020-10-29 11:59:37 +0000

    Add project files.

---
+++ Chess-console-mode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_console_mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
